f1e1f49b8acc507b7246a407d4cf12bfa2c0b951
Sauravkuntal <146329164+Sauravkuntal@users.noreply.github.com>
2023-09-29 22:45:39 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int 
+int main ()
+{
+short a=1024;
+short b=64;
+short c=a*b;
+cout<<a<<" "<<b<<endl;
+cout<<c<<endl;
+int x=5;
+char y='d';
+int sum1=x+y;
+cout<<sum1;
+
+
+    return 0;
+}
